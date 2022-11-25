47e9907a5303db328747bcc865f36c55cd26eae4
himyjan <51815522+himyjan@users.noreply.github.com>
2022-11-25 21:41:41 +0800

    remove package meta

--- android/app/src/main/kotlin/com/example/flash_chat_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flash_chat_flutter/MainActivity.kt
@@ -5,4 +5,6 @@ import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity: FlutterActivity() {
+  // You can keep this empty class or remove it. Plugins on the new embedding
+  // now automatically registers plugins.
 }
