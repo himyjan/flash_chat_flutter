0b237e675530c08b4ca6f7fe599fd247fe3de279
himyjan <51815522+himyjan@users.noreply.github.com>
2022-11-26 00:05:41 +0800

    fix build

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
@@ -7,7 +8,9 @@ import 'package:flash_chat/screens/all_users_screen.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flash_chat/firebase/user_repository.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(FlashChat());
 }
 
